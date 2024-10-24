edf6bababe94cdd4f669345823811b7f3e8ceb65
A-Kantor <kantor.lexi@gmail.com>
2024-10-24 14:50:42 -0400

    Data fetchingbut can't really test it yet

--- spotifywrapped/views.py
+++ spotifywrapped/views.py
@@ -84,6 +84,85 @@ def fetch_spotify_wrap_data(access_token):
     else:
         return []  # Return an empty list if there's an error
 
+def fetch_spotify_wrapped_metrics(access_token):
+    headers = {'Authorization': f'Bearer {access_token}'}
+
+    # 1. Top 5 Genres
+    top_tracks_url = 'https://api.spotify.com/v1/me/top/tracks'
+    top_tracks_response = requests.get(top_tracks_url, headers=headers)
+    top_tracks = top_tracks_response.json()['items'] if top_tracks_response.status_code == 200 else []
+
+    # Extract artist IDs from top tracks to gather genres
+    artist_ids = {artist['id'] for track in top_tracks for artist in track['artists']}
+    genre_count = {}
+    for artist_id in artist_ids:
+        artist_url = f'https://api.spotify.com/v1/artists/{artist_id}'
+        artist_response = requests.get(artist_url, headers=headers)
+        if artist_response.status_code == 200:
+            genres = artist_response.json().get('genres', [])
+            for genre in genres:
+                genre_count[genre] = genre_count.get(genre, 0) + 1
+    top_5_genres = sorted(genre_count.items(), key=lambda x: x[1], reverse=True)[:5]
+
+    # 2. Total Minutes Listened
+    recently_played_url = 'https://api.spotify.com/v1/me/player/recently-played'
+    recent_tracks_response = requests.get(recently_played_url, headers=headers)
+    recent_tracks = recent_tracks_response.json().get('items', [])
+    total_minutes_listened = sum(track['track']['duration_ms'] / 60000 for track in recent_tracks)
+
+    # 3. Top 5 Artists
+    top_artists_url = 'https://api.spotify.com/v1/me/top/artists'
+    top_artists_response = requests.get(top_artists_url, headers=headers)
+    top_artists = top_artists_response.json()['items'][:5] if top_artists_response.status_code == 200 else []
+
+    # 4. Number of Minutes Listened to Top Artist
+    minutes_listened_to_top_artist = 0
+    if top_artists:
+        top_artist_id = top_artists[0]['id']
+        minutes_listened_to_top_artist = sum(track['duration_ms'] / 60000 for track in top_tracks if top_artist_id in [artist['id'] for artist in track['artists']])
+
+    # 5. Total Number of Artists Listened To
+    total_artists_listened_to = len(artist_ids)
+
+    # 6. Top 5 Albums
+    album_count = {}
+    for track in top_tracks:
+        album = track['album']
+        album_count[album['id']] = album_count.get(album['id'], 0) + 1
+    top_5_albums = sorted(album_count.items(), key=lambda x: x[1], reverse=True)[:5]
+
+    # 7. Total Number of Albums Listened To
+    total_albums_listened_to = len(album_count)
+
+    # 8. Number of Times Top Album Was Played
+    top_album_id = top_5_albums[0][0] if top_5_albums else None
+    times_top_album_played = 0
+    if top_album_id:
+        times_top_album_played = sum(1 for track in top_tracks if track['album']['id'] == top_album_id)
+
+    # 9. Top 10 Songs
+    top_10_songs = top_tracks[:10]
+
+    # 10. Number of Times Listened to Top Song
+    top_song_id = top_tracks[0]['id'] if top_tracks else None
+    times_listened_to_top_song = 0
+    if top_song_id:
+        times_listened_to_top_song = sum(1 for track in recent_tracks if track['track']['id'] == top_song_id)
+
+    # Return all the metrics
+    return {
+        'top_5_genres': top_5_genres,
+        'total_minutes_listened': total_minutes_listened,
+        'top_5_artists': top_artists,
+        'minutes_listened_to_top_artist': minutes_listened_to_top_artist,
+        'total_artists_listened_to': total_artists_listened_to,
+        'top_5_albums': top_5_albums,
+        'total_albums_listened_to': total_albums_listened_to,
+        'times_top_album_played': times_top_album_played,
+        'top_10_songs': top_10_songs,
+        'times_listened_to_top_song': times_listened_to_top_song
+    }
+
 @login_required
 def get_top_tracks(request):
     wraps = SpotifyWrap.objects.filter(user=request.user)
@@ -106,4 +185,4 @@ def delete_account(request):
     return redirect('index')  # Redirect to a landing page or confirmation page
 
 def contact_developers(request):
-    return render(request, 'spotify/contact.html')
+    return render(request, 'spotify/contBct.html')
