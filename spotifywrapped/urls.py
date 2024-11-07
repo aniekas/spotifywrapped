8061e899461707aa6ef5f3a4be8624f230891f86
Anieka Seemakurti <136751807+aniekas@users.noreply.github.com>
2024-11-07 14:25:18 -0500

    fixed top artists

--- spotifywrapped/urls.py
+++ spotifywrapped/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path("wraps/", views.wrap_list, name="wrap_list"),
     path("index/", views.index, name="index"),
     path('wraps/<int:wrap_id>/', views.wrap_detail, name='wrap_detail'),
+path("wrap/delete/<int:wrap_id>/", views.delete_wrap, name="delete_wrap"),
 ]
